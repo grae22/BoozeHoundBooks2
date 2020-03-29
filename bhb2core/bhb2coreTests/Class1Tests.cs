87f09f15d8bab27e74a6b5505ce788beff5d0467
GraemeB <grae22@gmail.com>
2020-03-29 13:14:05 +0200

    Create core & core test projects.

---
+++ bhb2core/bhb2coreTests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using bhb2core;
+
+using NUnit.Framework;
+
+namespace bhb2coreTests
+{
+  [TestFixture]
+  public class Class1Tests
+  {
+    [Test]
+    public void MethodName_GivenCondition_ShouldExpected()
+    {
+      // Arrange.
+      var testObject = new Class1();
+
+      // Act.
+
+      // Assert.
+      Assert.NotNull(testObject);
+    }
+  }
+}
